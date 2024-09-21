76c8099716da14494e878d9cba537c0005a1523b
cloodei <nguyenan.study@gmail.com>
2024-09-21 18:53:25 +0700

    CRUD Invoices Commit

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -1,5 +1,6 @@
 import { PencilIcon, PlusIcon, TrashIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import { deleteInvoice } from '@/app/lib/actions';
 
 export function CreateInvoice() {
   return (
@@ -16,21 +17,24 @@ export function CreateInvoice() {
 export function UpdateInvoice({ id }: { id: string }) {
   return (
     <Link
-      href="/dashboard/invoices"
-      className="rounded-md border p-2 hover:bg-gray-100"
+      href={`/dashboard/invoices/${id}/edit`}
+      className="rounded-md border p-2 bg-teal-300 hover:bg-teal-600"
     >
       <PencilIcon className="w-5" />
     </Link>
   );
 }
-
+ 
 export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+
   return (
-    <>
-      <button className="rounded-md border p-2 hover:bg-gray-100">
+    // hover:bg-gray-100 (for btns)
+    <form action={deleteInvoiceWithId}>
+      <button className="rounded-md border p-2 bg-red-400 hover:bg-red-700">
         <span className="sr-only">Delete</span>
         <TrashIcon className="w-5" />
       </button>
-    </>
+    </form>
   );
 }
