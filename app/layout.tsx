f77fd4190ad330fc0a409a90e3f5cf6ca7a16f7d
cloodei <nguyenan.study@gmail.com>
2024-09-21 19:39:01 +0700

    Analytics Implementation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { Analytics } from "@vercel/analytics/react";
 import { inter } from '@/app/ui/fonts';
 
 export default function RootLayout({
@@ -11,7 +12,10 @@ export default function RootLayout({
       <head>
         <title>aan NextJS</title>
       </head>
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
